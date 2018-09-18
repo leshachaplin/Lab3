cd74b35246c9d920a82765754ab28894187d80ac
Lesha Chaplin <lesha.chaplin66@gmail.com>
2018-09-18 21:20:02 +0300

    add all files

--- src/Lab3.java
+++ src/Lab3.java
@@ -1,3 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Lab3 {
 
+    public static String[](String nameOfFile){
+        try (FileReader fr = new FileReader(nameOfFile)) {
+            BufferedReader isr = new BufferedReader(fr);
+        }
+
+    }
+
+    public static void main(String[] args){
+
+    }
 }
